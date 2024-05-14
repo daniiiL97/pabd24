019a5cfc4079c40e4b0a4b221dbd53179d384914
Labintsev A I <andrej.labintsev@yandex.ru>
2024-05-15 01:23:38 +0300

    Update results.csv

--- src/parse_cian.py
+++ src/parse_cian.py
@@ -1,19 +1,35 @@
+"""  Parse data from cian.ru
+https://github.com/lenarsaitov/cianparser
+"""
+import datetime
+
 import cianparser
+import pandas as pd
 
 moscow_parser = cianparser.CianParser(location="Москва")
 
 
-def main():
+def main(n_rooms):
+    """
+    Function docstring
+    """
+    t = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
+    csv_path = f'../data/raw/{n_rooms}_{t}.csv'
     data = moscow_parser.get_flats(
         deal_type="sale",
-        rooms=(2,),
-        with_saving_csv=True,
+        rooms=(n_rooms,),
         additional_settings={
             "start_page": 1,
             "end_page": 50,
             "object_type": "secondary"
         })
+    df = pd.DataFrame(data)
+
+    df.to_csv(csv_path,
+              encoding='utf-8',
+              index=False)
 
 
 if __name__ == '__main__':
-    main()
+    for i in range(1,4):
+        main(i)
